d3c0ec039042bd5eaa4948ee12b8701bb0dfbf6f
TeodorDyakov <tedy1996@gmail.com>
2020-12-03 00:25:27 +0200

    add tools folder for xml validator and xslt processor

--- course_project/build.sh
+++ course_project/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-xmllint --schema HRManagementSystem.xsd HRManagementSystem.xml --noout
-xsltproc slujiteli.xsl HRManagementSystem.xml > slujiteli.html
-xsltproc otdeli.xsl HRManagementSystem.xml > otdeli.html
+./tools/xmllint --schema HRManagementSystem.xsd HRManagementSystem.xml --noout
+./tools/xsltproc slujiteli.xsl HRManagementSystem.xml > slujiteli.html
+./tools/xsltproc otdeli.xsl HRManagementSystem.xml > otdeli.html
