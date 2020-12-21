66cfae5f179d84aacf60833e94caef415cc9ccfa
TeodorDyakov <tedy1996@gmail.com>
2020-12-21 21:36:16 +0200

    only project

---
+++ course_project/dots.go
@@ -0,0 +1,16 @@
+import "fmt"
+
+func() print{
+
+}
+
+func() main{
+	var a [5][5] int
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter x coord: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Print("Enter y coord: ")
+
+	fmt.Println("hello")
+}
